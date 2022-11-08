b131fdf69638c90fe6d0e659f4c37aa2cb319026
Lucas Oliveira <lucasrodriguesmano@gmail.com>
2022-11-07 23:19:39 -0300

    Finalizando o mobile

--- mobile/src/screens/Details.tsx
+++ mobile/src/screens/Details.tsx
@@ -1,13 +1,15 @@
+import { useEffect, useState } from "react";
+import { Share } from "react-native";
 import { HStack, useToast, VStack } from "native-base";
 import { useRoute } from "@react-navigation/native";
 import { api } from "../services/api";
 import { Header } from "../components/Header";
-import { useEffect, useState } from "react";
 import { Loading } from "../components/Loading";
-import { Option } from "../components/Option";
 import { PoolCardProps } from '../components/PoolCard';
 import { PoolHeader } from "../components/PoolHeader";
 import { EmptyMyPoolList } from "../components/EmptyMyPoolList";
+import { Option } from "../components/Option";
+import { Guesses } from '../components/Guesses';
 
 interface RouteParams{
   id: string;
@@ -17,28 +19,34 @@ export function Details(){
   const [optionSelected, setOptionSelected] = useState<'guesses' | 'ranking'>('guesses');
   const [isLoading, setIsLoading] = useState(true);
   const [poolDetails, setPoolDetails] = useState<PoolCardProps>({} as PoolCardProps);
+
   const route = useRoute();
   const toast = useToast();
+
   const { id } = route.params as RouteParams;
 
   async function fetchPoolDetails(){
     try{
       setIsLoading(true);
 
-      const response = await api.get('/pools/${id');
+      const response = await api.get(`/pools/${id}`);
       setPoolDetails(response.data.pool);
-
+    }catch (error){
+      console.log(error);
       toast.show({
         title: 'Não foi possível carregar os detalhes do bolão',
         placement: 'top',
         bgColor: 'red.500'
       });
-    }catch (error){
-      console.log()
     }finally{
       setIsLoading(false);
     }
-    
+  }
+
+  async function handleCodeShare(){
+    await Share.share({
+      message: poolDetails.code
+    })
   }
 
   useEffect(() => {
@@ -53,27 +61,39 @@ export function Details(){
 
   return(
     <VStack flex={1} bgColor="gray.900">
-      <Header title="Título do Bolão" showBackButton showShareButton/>
+      <Header 
+      title={poolDetails.title} 
+      showBackButton 
+      showShareButton
+      onShare={handleCodeShare}
+      />
+
       {
         poolDetails._count?.participants > 0 ?
         <VStack px={5} flex={1}>
           <PoolHeader data={poolDetails}/>
-          <HStack bgColor="gray.800" p={1} rounded="sm" mb={5}>
+
+          <HStack bgColor="gray.800" p={1} rounded="sm" mb={8}>
             <Option 
-            title="guesses" 
+            title="Seus palpites" 
             isSelected={optionSelected === 'guesses'}
             onPress={() => setOptionSelected('guesses')}
             />
 
             <Option 
-            title="ranking" 
+            title="Ranking do grupo" 
             isSelected={optionSelected === 'ranking'}
             onPress={() => setOptionSelected('ranking')}
             />
           </HStack>
+
+          <Guesses 
+            poolId={poolDetails.id} 
+            code={poolDetails.code} 
+            />
         </VStack>
         : <EmptyMyPoolList code={poolDetails.code}/>
       }
     </VStack>
-  )
+  );
 }
